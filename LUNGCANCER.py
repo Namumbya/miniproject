c11f1acd56ea5735bd1aaf6d22fc1463c6a789af
Mercy Diana <mercydiananamumbya@gmail.com>
2023-02-27 01:57:39 +0300

    modification

--- LUNGCANCER.py
+++ LUNGCANCER.py
@@ -54,28 +54,27 @@ def predict_app():
     YELLOW_FINGERS = st.radio("Do you have yellow fingers or nails?", ["Yes", "No"],index=None)
     # Preprocess the features only if the user has selected an option
     #if GENDER is not None and TOBACCO_SMOKING is not None and ALLERGY is not None and FATIGUE is not None and COUGHING is not None and ALCOHOL_CONSUMING is not None and WHEEZING is not None and SHORTNESS_OF_BREATH is not None and CHEST_PAIN is not None and SWALLOWING_DIFFICULTY is not None and CHRONIC_DISEASE is not None and YELLOW_FINGERS is not None:
-    inputs = [AGE, GENDER, TOBACCO_SMOKING, ALLERGY, FATIGUE, COUGHING, ALCOHOL_CONSUMING, WHEEZING, SHORTNESS_OF_BREATH, CHEST_PAIN, SWALLOWING_DIFFICULTY, CHRONIC_DISEASE, YELLOW_FINGERS]
 
   
      #Add a prediction button
     if st.button("Predict"):
-        if validate_inputs(inputs):
-          # Preprocess the features
+        
+        # Preprocess the features
      
-           features = [
-              AGE,
-              1 if GENDER == "Male" else 0,
-              1 if TOBACCO_SMOKING == "Yes" else 0,
-              1 if ALLERGY== "Yes" else 0,
-              1 if FATIGUE == "Yes" else 0,
-              1 if COUGHING == "Yes" else 0,
-              1 if ALCOHOL_CONSUMING == "Yes" else 0,
-              1 if WHEEZING == "Yes" else 0,
-              1 if SHORTNESS_OF_BREATH == "Yes" else 0,
-              1 if CHEST_PAIN == "Yes" else 0,
-              1 if SWALLOWING_DIFFICULTY == "Yes" else 0,
-              1 if CHRONIC_DISEASE == "Yes" else 0,
-              1 if YELLOW_FINGERS == "Yes" else 0
+        features = [
+            AGE,
+            1 if GENDER == "Male" else 0,
+            1 if TOBACCO_SMOKING == "Yes" else 0,
+            1 if ALLERGY== "Yes" else 0,
+            1 if FATIGUE == "Yes" else 0,
+            1 if COUGHING == "Yes" else 0,
+            1 if ALCOHOL_CONSUMING == "Yes" else 0,
+            1 if WHEEZING == "Yes" else 0,
+            1 if SHORTNESS_OF_BREATH == "Yes" else 0,
+            1 if CHEST_PAIN == "Yes" else 0,
+            1 if SWALLOWING_DIFFICULTY == "Yes" else 0,
+            1 if CHRONIC_DISEASE == "Yes" else 0,
+            1 if YELLOW_FINGERS == "Yes" else 0
         ]
 
         # Make the prediction
